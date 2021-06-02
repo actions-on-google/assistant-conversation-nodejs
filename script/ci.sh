597190f789ac2b83fc0422a9bb18a79c491aa920
Nick Felker <fleker@google.com>
2021-06-02 14:56:16 -0400

    Add CHANGELOG.md and trailing space checks in CI script
    Fix CI script execution
    DISABLE_CHANGELOG_CHECK=Meta change
    
    Bug: 188656105
    Change-Id: If29c42d6bf58059a84604f59fc9df31476ff83bd

--- script/ci.sh
+++ script/ci.sh
@@ -20,12 +20,15 @@ if [ "$NODE_ENV" != "ci" ]; then
   exit 1
 fi
 
+# Fail on any error.
+set -e
+set -x
+
+# Install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 # Load nvm
 export NVM_DIR="$HOME/.nvm"
 . "$NVM_DIR/nvm.sh"
-
-# Fail on any error.
-set -e
 
 # Test on Node.js 10.18.0
 echo "Running tests on Node 10.18.0"
@@ -52,3 +55,24 @@ npm i -g yarn
 # Actual build
 yarn
 yarn ci
+
+# Setup Git Presubmit Linter
+GIT_PRESUBMIT_LINTER='./git-presubmit-linter'
+RULES="${GIT_PRESUBMIT_LINTER}/rules"
+
+if [ ! -d ${GIT_PRESUBMIT_LINTER} ]; then
+    # Clone the git presubmit linter repository
+    git clone https://github.com/google/git-presubmit-linter.git
+fi
+cd ${GIT_PRESUBMIT_LINTER}
+git pull origin master
+cd ../
+
+# Check that CHANGELOG.md was updated (unless explicitly turned off)
+git log -1 | grep "DISABLE_CHANGELOG_CHECK"
+DISABLE_CHANGELOG_CHECK=$?
+if [ $DISABLE_CHANGELOG_CHECK -ne 0 ]; then
+    git diff HEAD~1 --name-only | ${RULES}/has-pattern.sh "CHANGELOG.md"
+fi
+# Check that no change introduces trailing whitespace
+git diff HEAD~1 --pretty=%B | ${RULES}/trailing-whitespace.sh
